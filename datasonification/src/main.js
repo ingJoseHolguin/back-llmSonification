9ed852fa12be8ad0e950420abf05e66b71902fff
ingJoseHolguin <jp.holguin95@gmail.com>
2025-03-03 20:28:31 -0600

    add

--- datasonification/src/main.js
+++ datasonification/src/main.js
@@ -2,10 +2,16 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+//import { Chart } from "highcharts-vue";
+import Highcharts from "highcharts";
+import sonificationModule from "highcharts/modules/sonification";
+
+sonificationModule(Highcharts);
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+//app.component("highchartsVue", Chart);
 
 app.mount('#app');
