f279310f07473947432c9765f298beaba8839773
ingJoseHolguin <jp.holguin95@gmail.com>
2025-08-14 16:11:09 -0600

    limpiando

--- sonification-studio/src/app/editor/core/utils/sonificationTools.ts
+++
@@ -1,4 +0,0 @@
-
-export function speedToDuration(speed: number): number {
-    return Math.round(1 / Math.pow(speed, 0.4 * speed / 100 + 0.4) * 320000 - 6000);
-}
--- sonification/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import store from './store'; // Importar la store
-
-const app = createApp(App);
-
-app.use(store); // Registrar la store en la aplicación
-
-app.mount('#app');
